1d9663954c66cbcfabe08621a07ec1d400bc0046
quanvu72 <quan.vu2212829@hcmut.edu.vn>
2025-11-15 14:04:06 +0700

    0.1

---
+++ pub_on.py
@@ -0,0 +1,16 @@
+from paho.mqtt import client as mqtt
+import asyncio
+
+
+broker = "192.168.18.129"
+data_test = "green_on"
+
+client = mqtt.Client(client_id="1236", clean_session=True)
+client.connect(broker, 1883)
+    
+async def main():
+    client.publish("/led_control", data_test)
+    await asyncio.sleep(4)
+
+asyncio.run(main())
+client.disconnect()
